19422cf1d0ed22df5a14b2793a59d98e1889153b
Rawal <rawaldallakoti@gmail.com>
2024-03-29 16:02:39 +0545

    Implemented routes and controllers of brand and category

--- api/controllers/cms/index.js
+++ api/controllers/cms/index.js
@@ -1,5 +1,9 @@
 const Staff = require("./staff.controller");
+const Brand = require("./brand.controller");
+const Category = require("./categories.controller");
 
 module.exports = {
   Staff,
+  Brand,
+  Category,
 };
--- api/routes/cms/index.js
+++ api/routes/cms/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const staffRoute = require("./staff.route");
+const brandRoute = require("./brand.route");
+const categoryRoute = require("./category.route");
 const { adminOnly } = require("../../middleware");
 
 router.use("/staffs", adminOnly, staffRoute);
+router.use("/brands", brandRoute);
+router.use("/categories", categoryRoute);
 
 module.exports = router;
