079a0d1e6df9c838b08d64bbb6882fe3610e580e
Rawal <rawaldallakoti@gmail.com>
2024-04-01 17:23:58 +0545

    Impelemented routes and controller of review and orders (CMS)

--- api/controllers/cms/index.js
+++ api/controllers/cms/index.js
@@ -2,10 +2,14 @@ const Staff = require("./staff.controller");
 const Brand = require("./brand.controller");
 const Category = require("./categories.controller");
 const Product = require("./products.controller");
+const Review = require("./review.controller");
+const Order = require("./order.controller");
 
 module.exports = {
   Staff,
   Brand,
   Category,
   Product,
+  Review,
+  Order,
 };
--- api/routes/cms/index.js
+++ api/routes/cms/index.js
@@ -4,11 +4,16 @@ const staffRoute = require("./staff.route");
 const brandRoute = require("./brand.route");
 const categoryRoute = require("./category.route");
 const productRoute = require("./product.route");
+const orderRoute = require("./order.route");
+const reviewRoute = require("./review.route");
 const { adminOnly } = require("../../middleware");
 
 router.use("/staffs", adminOnly, staffRoute);
 router.use("/brands", brandRoute);
 router.use("/categories", categoryRoute);
+router.use("/orders", orderRoute);
+router.use("/reviews", reviewRoute);
+
 router.use("/products", productRoute);
 
 module.exports = router;
