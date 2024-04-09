23fdbc355101aa26d44b753c27f40b50ae417dfe
Rawal <rawaldallakoti@gmail.com>
2024-04-09 11:16:52 +0545

    Fix bugs in CartBtn component to ensure correct calculation of total price.

--- front/src/components/CartBtn.jsx
+++ front/src/components/CartBtn.jsx
@@ -7,32 +7,36 @@ export const CartBtn = ({ product, qty = 1 }) => {
   const cart = useSelector(state => state.cart.value);
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
-  // console.log(cart);
+
   const handleCart = () => {
     setLoading(true);
-    let id = product._id;
-    let qt = qty;
-    let price =
+    const id = product._id;
+    const price =
       product.discounted_price > 0 ? product.discounted_price : product.price;
     let total = price * qty;
+    let newQty = qty;
+
     if (id in cart) {
-      qt += cart[id]["qty"];
-      total += price * qt;
+      // Product already in cart, update quantity and total
+      newQty += cart[id]["qty"];
+      total = price * newQty;
     }
-    dispatch(
-      setCart({
-        ...cart,
-        [id]: {
-          product,
-          qty: qt,
-          total,
-          price,
-        },
-      })
-    );
+
+    const updatedCart = {
+      ...cart,
+      [id]: {
+        product,
+        qty: newQty,
+        total,
+        price,
+      },
+    };
+
+    dispatch(setCart(updatedCart));
     setLoading(false);
     toast.success("Product added to cart");
   };
+
   return (
     <button className="btn btn-outline-dark" type="button" onClick={handleCart}>
       <i
